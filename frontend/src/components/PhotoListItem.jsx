128c04bcf34082e3a4d501119e3bb3b800c859e6
Garrick Henne <garrickhenne@outlook.com>
2023-10-31 14:55:46 -0700

    Render photo list with some photos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
+  const { urls : { regular } , user: { name, profile }, location : { city, country } } = props.data;
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <article className="photo-list__item">
+      <img src={ regular } className="photo-list__image" />
+      <div className=".photo-list__user-info photo-list__user-details">
+        <img src={ profile } className="photo-list__user-profile"/>
+        <div className="photo-list__user-info">
+          { name }
+          <div className="photo-list__user-location">{ city }, { country }</div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default PhotoListItem;
