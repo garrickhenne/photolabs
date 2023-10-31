a20dcb6c86559cacbbfaa800ec779fb9493a5b52
Garrick Henne <garrickhenne@outlook.com>
2023-10-31 15:33:48 -0700

    Build top nav bar full

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import TopicList from './TopicList';
+import topics from 'mocks/topics';
+import FavBadge from './FavBadge';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList data={ topics }/>
+      <FavBadge selected={ true }/>
     </div>
   )
 }
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = (props) => {
+  const formattedTopics = props.data.map(topic => {
+    return <li key={ topic.id }><TopicListItem data={ topic }/></li>
+  })
 
-const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      { formattedTopics }
     </div>
   );
 };
