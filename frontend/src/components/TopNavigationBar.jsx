936ba0dbb9daa33c405275a456f6f665995098b7
Garrick Henne <garrickhenne@outlook.com>
2023-11-05 20:06:01 -0800

    Add onClick to logo to refresh

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -7,7 +7,7 @@ import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo" onClick={ () => window.location.reload() }>PhotoLabs</span>
       <TopicList data={ props.topics } handleTopicClick={ props.handleTopicClick }/>
       <FavBadge selected={ true } isFavPhotoExist={ props.likes.length }/>
     </div>
